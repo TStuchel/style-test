1a523ddda7ce0214383635d39e1150f024bdc497
Tony Stuchel <tony@stuchel.com>
2018-12-04 16:12:31 -0500

    Initial commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,13 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
+import Card from './components/card/Card';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+        <Card text="A" x="100" y="0" background='green'/>
+        <Card text="B" x="300" y="0" background='red'/>
       </div>
     );
   }
---
+++ src/components/card/Card.js
@@ -0,0 +1,68 @@
+import React, { Component } from 'react';
+import glamorous from 'glamorous';
+import './Card.scss'
+
+const CardFrame = glamorous.div((props) => ({
+    position: 'absolute',
+    display: 'flex',
+    height: '200px',
+    width: '150px',
+    verticalAlign: 'middle',
+    color: 'white',
+    borderRadius: '10px',
+    margin: 'auto',
+    cursor: 'pointer',
+    userSelect: 'none',
+    border: '2px solid',
+
+    borderColor: props.isHeld ? 'black' :  props.background,
+    background: props.background,
+    zIndex: props.isHeld ? 1000 : 0,
+    top: props.y,
+    left: props.x,
+}))
+
+const CardText = glamorous.p({
+    margin: 'auto'
+})
+
+export default class Card extends Component {
+
+    constructor(props) {
+        super(props)
+        this.state = {
+            x: props.x,
+            y: props.y,
+            isHeld: false
+        }
+    }
+
+    handleMouseDown = (event) => {
+        this.setState({
+            holdX: event.clientX - this.state.x,
+            holdY: event.clientY - this.state.y,
+            isHeld: true
+        })
+    }
+
+    handleMouseUp = (event) => {
+        this.setState({ isHeld: false })
+    }
+
+    handleMouseMove = (event) => {
+        if (this.state.isHeld) {
+            this.setState({ x: event.clientX - this.state.holdX })
+            this.setState({ y: event.clientY - this.state.holdY })
+        }
+    }
+
+    render() {
+        return (
+            <CardFrame className="cardFrame" background={this.props.background} x={this.state.x} y={this.state.y} onMouseDown={this.handleMouseDown} onMouseUp={this.handleMouseUp} onMouseMove={this.handleMouseMove} isHeld={this.state.isHeld}>
+                <CardText>
+                    {this.props.text}
+                </CardText>
+            </CardFrame>
+        )
+    }
+}
